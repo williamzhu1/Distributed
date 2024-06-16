22b77e5f157dbe496cefa7813e0976ad218db3a3
williamzhu1 <william.zhu@sstudent.kuleuven.be>
2024-06-16 05:59:13 +0200

    gg

--- src/main/resources/requests/new-item-restrpc.sh
+++ src/main/resources/requests/new-item-restrpc.sh
@@ -1,2 +1,2 @@
-curl -X GET localhost:8080/items -H 'Content-Type: application/json' -H 'Apikey: 292d594b-ff36-4ba4-9574-d441ce9e9ee5'
+curl -X GET localhost:8080/items  -H 'Apikey: 292d594b-ff36-4ba4-9574-d441ce9e9ee5'
 
