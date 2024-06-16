28f50b2192ae1926e381f6575f45f4addb669102
williamzhu1 <william.zhu@sstudent.kuleuven.be>
2024-06-16 23:21:10 +0200

    cors

--- src/main/java/be/kuleuven/dsgt4/WebConfig.java
+++ src/main/java/be/kuleuven/dsgt4/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // Replace with your frontend's origin
+                .allowedOrigins("https://distributed-426520.ew.r.appspot.com/") // Replace with your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
