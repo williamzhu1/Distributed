71231bbd71aaa0be9c9bc6637513d7b343eeeef4
williamzhu1 <william.zhu@sstudent.kuleuven.be>
2024-06-16 05:50:54 +0200

    gg

--- src/main/resources/requests/new-item-restrpc.sh
+++ src/main/resources/requests/new-item-restrpc.sh
@@ -1,2 +1,2 @@
-curl -X POST http://20.52.252.170:8081/items -H 'Content-Type: application/json' -H 'Apikey: 1e9d691b-a8a5-439e-af2f-6d88400f572d' -d @new-meal.json -v
+curl -X GET localhost:8080/items -H 'Content-Type: application/json' -H 'Apikey: 292d594b-ff36-4ba4-9574-d441ce9e9ee5'
 
